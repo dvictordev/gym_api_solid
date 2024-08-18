7d5f607e3730d69575dc9527a6853fb36b79ab6d
victor duarte <victor.d.dev@gmail.com>
2024-08-18 11:15:43 -0300

    feat: :sparkles: add jwt authentication middleware

--- src/http/controller/profile.controller.ts
+++ src/http/controller/profile.controller.ts
@@ -1,11 +1,21 @@
+import { makeGetUserProfielUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyRequest, FastifyReply } from "fastify";
+import { openAsBlob } from "fs";
 
 export async function profileController(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
   await request.jwtVerify();
-  console.log(request.user);
 
-  return reply.status(200).send("Authenticated");
+  const getUserProfile = makeGetUserProfielUseCase();
+
+  const { user } = await getUserProfile.execute({ userId: request.user.sub });
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  });
 }
