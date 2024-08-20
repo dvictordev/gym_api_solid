e9103263b408a09a0b5af97c99f816bcf9e5bc54
victor duarte <victor.d.dev@gmail.com>
2024-08-20 03:08:19 -0300

    fix: prisma-test-enviroment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,16 +1,15 @@
 import "dotenv/config";
-import { randomUUID } from "crypto";
+
+import { randomUUID } from "node:crypto";
 import { execSync } from "node:child_process";
 import { Environment } from "vitest";
 import { PrismaClient } from "@prisma/client";
 
-// postgresql://dockerpg:dockerpg@localhost:5432/apisolid?schema=public
-
 const prisma = new PrismaClient();
 
-function generateDatabaseUrl(schema: string) {
+function generateDatabaseURL(schema: string) {
   if (!process.env.DATABASE_URL) {
-    throw new Error("Please provide a database url environment variable");
+    throw new Error("Please provide a DATABASE_URL environment variable.");
   }
 
   const url = new URL(process.env.DATABASE_URL);
@@ -24,17 +23,16 @@ export default <Environment>{
   name: "prisma",
   async setup() {
     const schema = randomUUID();
+    const databaseURL = generateDatabaseURL(schema);
 
-    const databaseUrl = generateDatabaseUrl(schema);
-
-    process.env.DATABASE_URL = databaseUrl;
+    process.env.DATABASE_URL = databaseURL;
 
     execSync("npx prisma migrate deploy");
 
     return {
       async teardown() {
         await prisma.$executeRawUnsafe(
-          `DROP SCHEMA IF EXISTS "${schema} CASCADE"`
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
         );
 
         await prisma.$disconnect();
